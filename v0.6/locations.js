1d2168e40bdad1958ca3bd34cad69c475d174932
Anthony Giorgio <agiorgio@us.ibm.com>
2016-07-05 07:56:26 -0400

    Added new locations.

--- v0.6/locations.js
+++ v0.6/locations.js
@@ -33,17 +33,28 @@ function Location(_id, _name, _description) {
 }
 
 var northPole = new Location(0, "North Pole", "Brrr!  It's cold here!");
-var theSouth = new Location(1, "The South", "Welcome, y'all");
+var theSouth = new Location(1, "The South", "Welcome, y'all.");
 var outWest = new Location(2, "Out West", "Go west, young man.");
 var eastCoast = new Location(3, "East Coast", "You can see the Atlantic Ocean.");
-var forest = new Location(4, "Forest", "The trees are dark and spooky here");
-var desert = new Location(5, "Desert", "It's hot and dry here");
+var forest = new Location(4, "Forest", "The trees are dark and spooky here.");
+var desert = new Location(5, "Desert", "It's hot and dry here.");
+var castle = new Location(6, "Castle", "A stone castle looms above you.");
+var lake = new Location(7, "Lake",
+		"The azure waters of the lake look refreshing.");
+var hills = new Location(8, "Hills", "Green hills roll into the distance.");
+var plains = new Location(9, "Plains",
+		"Tumbleweeds blow across the empty plains.");
 
 northPole.validMoves = [ BAD, forest.id, BAD, BAD ];
-theSouth.validMoves = [ desert.id, BAD, BAD, BAD ];
-outWest.validMoves = [ BAD, BAD, desert.id, BAD ];
-eastCoast.validMoves = [ BAD, BAD, BAD, desert.id ];
+theSouth.validMoves = [ desert.id, BAD, castle.id, lake.id ];
+outWest.validMoves = [ plains.id, BAD, desert.id, BAD ];
+eastCoast.validMoves = [ BAD, hills.id, BAD, desert.id ];
 forest.validMoves = [ northPole.id, desert.id, BAD, BAD ];
 desert.validMoves = [ forest.id, theSouth.id, eastCoast.id, outWest.id ];
+castle.validMoves = [ BAD, BAD, BAD, theSouth.id ];
+lake.validMoves = [ BAD, BAD, theSouth.id, BAD ];
+hills.validMoves = [ eastCoast.id, BAD, BAD, BAD ];
+plains.validMoves = [ BAD, outWest.id, BAD, BAD ];
 
-var locations = [ northPole, theSouth, outWest, eastCoast, forest, desert ];
+var locations = [ northPole, theSouth, outWest, eastCoast, forest, desert,
+		castle, lake, hills, plains ];
